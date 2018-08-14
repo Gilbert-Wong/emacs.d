df37684d009961222d2eb2aef654fce92a57a061
Gilbert Wong <gilbertwong96@icloud.com>
2018-08-14 19:56:31 +0800

    Temporary fix error in Counsel projectile

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -2,13 +2,17 @@
 ;;; Commentary:
 ;;; Code:
 
+(setq projectile-keymap-prefix (kbd "C-c C-p"))
+
 (setq projectile-enable-caching t)
 
 (global-set-key (kbd "C-c p f") 'counsel-projectile-find-file)
 
-(global-set-key (kbd "C-c p p") 'projectile-switch-project)
+(global-set-key (kbd "C-c p p") 'counsel-projectile-switch-project)
 
 (global-set-key (kbd "C-c p a") 'counsel-projectile-ag)
+
+(global-set-key (kbd "C-c p r") 'counsel-projectile-rg)
 
 (global-set-key (kbd "C-c p g") 'counsel-projectile-grep)
 
