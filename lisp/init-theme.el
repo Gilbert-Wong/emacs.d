c2e283a120fc92a1b6899c478b9af14221a40710
Gilbert Wong <gilbertwong96@icloud.com>
2018-08-14 11:43:33 +0800

    beautify neotree

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -1,13 +1,13 @@
-;;; packages init-theme
-;;; Commentary: appearance definition
+;;; package --- init-theme
+;;; Commentary:
 ;;; author gilbert wong, gilbertwong96@icloud.com
+;;; Code:
 
-
-;; Code
+(use-package all-the-icons)
 
 (use-package highlight-parentheses
-	     :config
-	     (global-highlight-parentheses-mode t))
+  :config
+  (global-highlight-parentheses-mode t))
 
 ;; (use-package all-the-icons
 ;;	     :config
@@ -25,6 +25,10 @@
 (set-cursor-color "White")
 (setq-default cursor-type 'bar)
 
+;; show column number
+(column-number-mode t)
+
+
 ;; hide tool bar
 (if window-system
     (tool-bar-mode 0))
@@ -33,13 +37,12 @@
 (scroll-bar-mode 0)
 (fringe-mode 0)
 
+(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+
 (setq inhibit-startup-message t)
 
 ;; font config
 (set-default-font "Inziu Iosevka SC 16")
 
-(setq neo-theme (if window-system 'icons 'arrow))
-
 (provide 'init-theme)
-
 ;;; init-theme.el ends here
