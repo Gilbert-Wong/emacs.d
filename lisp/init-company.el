92e98dd9846df65cfc4e41cbf26ce32680738363
Gilbert Wong <gilbertwong96@icloud.com>
2018-08-14 10:20:29 +0800

    Add Company config for elisp

---
+++ lisp/init-company.el
@@ -0,0 +1,31 @@
+;;; package --- init-company
+;;; Commentary:
+;;; Code:
+
+(global-company-mode t)
+(setq company-idle-delay 0.08)
+(setq company-minimum-prefix-length 1)
+(setq company-show-numbers t)
+(setq company-require-match nil)
+(setq company-dabbrev-downcase nil)
+
+(add-to-list 'company-backends '(company-keywords
+                                 company-files
+                                 company-abbrev
+                                 company-yasnippet
+                                 company-dabbrev
+                                 company-gtags
+                                 company-etags
+                                 company-eclim
+                                 company-elisp))
+
+(add-hook 'emacs-lisp-hook
+	  (lambda()
+            (setq (make-local-variable 'company-backends)
+                  '(company-elisp
+                    company-yasnippet
+                    company-abbrev
+                    company-dabbrev))))
+
+(provide 'init-company)
+;;; init-company.el ends here
