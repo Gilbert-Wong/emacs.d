04a5900cb8fafda2a45fc0e78db1f7c24061539f
Gilbert Wong <gilbertwong96@icloud.com>
2018-08-14 10:22:23 +0800

    Add format buffer and make code more standard

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -1,11 +1,20 @@
-;;; packages init-theme
-;;; Commentary: appearance definition
+;;; package --- init-utils
+;;; Commentary:
 ;;; author gilbert wong, gilbertwong96@icloud.com
+;;; Code:
 
 (defun hack-emacs ()
-  "config file"
+  "Hack Emacs Lisp files."
   (interactive)
   (find-file "~/.emacs.d/lisp/"))
+
+(defun format-buffer()
+  "Format code."
+  (interactive)
+  (indent-region (point-min) (point-max) nil)
+  )
+
+
 (provide 'init-utils)
 ;;; init-utils.el ends here
 
