99212064c1af499093aeb354435f5f0ba99d1370
Gilbert Wong <gilbertwong96@icloud.com>
2018-08-19 21:24:32 +0800

    Optimize ivy-erlang-complete

--- lisp/init-erlang.el
+++ lisp/init-erlang.el
@@ -33,24 +33,21 @@
   :hook
   ((erlang-mode . ivy-erlang-complete-init)
    (erlang-mode . company-erlang-init)
-   (erlang-mode . ivy-erlang-complete-reparse))
+   (after-save . ivy-erlang-complete-reparse))
   :config
   (progn (add-to-list 'auto-mode-alist '("rebar\\.config$" . erlang-mode))
-	 (add-to-list 'auto-mode-alist '("relx\\.config$" . erlang-mode))
-	 (add-to-list 'auto-mode-alist '("system\\.config$" . erlang-mode))
-	 (add-to-list 'auto-mode-alist '("\\.app\\.src$" . erlang-mode)))
+	     (add-to-list 'auto-mode-alist '("relx\\.config$" . erlang-mode))
+	     (add-to-list 'auto-mode-alist '("system\\.config$" . erlang-mode))
+	     (add-to-list 'auto-mode-alist '("\\.app\\.src$" . erlang-mode)))
   :bind
   (
-   ("C-c e d" . 'ivy-erlang-complete-find-definition)
    ("C-c e s" . 'ivy-erlang-complete-find-spec)
-   ("C-c e r" . 'ivy-erlang-complete-find-references)
    ("C-c e f" . 'ivy-erlang-complete-find-file)
    ("C-c e h" . 'ivy-erlang-complete-show-doc-at-point)
    ("C-c e p" . 'ivy-erlang-complete-set-project-root)
    ("C-c e a" . 'ivy-erlang-complete-autosetup-project-root)
    )
   )
-
 
 (provide 'init-erlang)
 ;;; init-erlang.el ends here
