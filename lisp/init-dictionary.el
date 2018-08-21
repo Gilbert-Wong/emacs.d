64f48ab56f59647f2034a19fdf61eebbec2d5f3c
Gilbert Wong <gilbertwong96@icloud.com>
2018-08-21 14:37:54 +0800

    fix youdao dict error

--- lisp/init-dictionary.el
+++ lisp/init-dictionary.el
@@ -2,7 +2,7 @@
 ;;; Commentary:
 ;;; Code:
 
-(global-set-key (kbd "C-c y") 'youdao-dictionary-search-at-Point+)
+(global-set-key (kbd "C-c y") 'youdao-dictionary-search-at-point+)
 
 (provide 'init-dictionary)
 ;;; init-dictionary.el ends here
